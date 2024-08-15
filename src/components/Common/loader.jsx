44ab1f53cb5d993db6e299d8cb04e79beefc75c0
unknown <jhobelsaha@gmail.com>
2024-08-15 22:23:13 +0600

    loding time excute

--- src/components/Common/loader.jsx
+++ src/components/Common/loader.jsx
@@ -45,13 +45,11 @@ function LoadingScreen() {
 
       <div className="loader-wrap-heading">
         <div className="load-text">
-          <span>L</span>
-          <span>o</span>
-          <span>a</span>
-          <span>d</span>
-          <span>i</span>
-          <span>n</span>
-          <span>g</span>
+          <span>H</span>
+          <span>E</span>
+          <span>N</span>
+          <span>R</span>
+          <span>Y</span>
         </div>
       </div>
     </div>
